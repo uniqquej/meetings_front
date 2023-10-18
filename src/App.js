20f8844c8848b53da9ef47245001346ba7831259
uniqquej <cjy4477@gmail.com>
2023-10-18 14:46:18 +0900

    feat: 로그인, 회원가입 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import {LoginPage, AuthPage, SignupPage} from './user/api';
+import PostAPI from './post/PostAPI';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path='/login' element={ <LoginPage />}/>
+          <Route path='/auth' element={ <AuthPage />}/>
+          <Route path='/signup' element={ <SignupPage />}/>
+          <Route path='/post' element={ <PostAPI />}/>
+        </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/user/api.js
@@ -0,0 +1,181 @@
+import "./api.css"
+
+import React, {useState} from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/"
+
+const LoginPage = ()=>{
+
+    const [PhoneNumber, setPhoneNumber] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const onPhoneNumberHandler = (event) => {
+        setPhoneNumber(event.currentTarget.value);
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+
+    const login = (phone_number, password) =>{
+        const userData = {
+            phone_number,
+            password
+        };
+        axios.post('user/login', userData).then(
+            response => {
+                const accessToken = response.data["access"];
+                localStorage.setItem('access', accessToken)
+    
+                axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+            }
+        ).catch(error=>{console.log(error)})
+    }
+
+    const onLogin = ()=>{
+        login(PhoneNumber, Password)
+    }
+
+
+    return(
+        <div className="inputBox">
+            <input className="form-control" type="text" value={PhoneNumber} 
+            onChange={onPhoneNumberHandler} placeholder="phone number"/>
+            <input className="form-control" type="password" value={Password} 
+            onChange={onPasswordHandler} placeholder="password"/>
+            <button className="btn btn-dark" onClick={onLogin}>로그인</button>
+            <button className="btn btn-dark">회원 가입</button>
+        </div>
+    )
+}
+
+const AuthPage = ()=>{
+    const navigate = useNavigate();
+
+    const [PhoneNumber, setPhoneNumber] = useState("");
+    const [AuthNumber, setAuthNumber] = useState("");
+    const [IsSmsSended, setIsSmsSended] = useState(false);
+
+    const onPhoneNumberHandler = (event) => {
+        setPhoneNumber(event.currentTarget.value);
+    }
+
+     const onAuthNumberHandler = (event) => {
+        setAuthNumber(event.currentTarget.value);
+    }
+
+    const sendAuthNumber = async (phone_number) =>{
+        const userData = {
+            phone_number,
+        };
+        const res = await axios.post('user/sms', userData);
+        
+        if (res.status === 202){
+            setIsSmsSended(true);
+        }
+    }
+
+    const onSendAuthNumber = ()=>{
+        sendAuthNumber(PhoneNumber)
+    }
+
+    const checkAuthNumber = async (PhoneNumber, AuthNumber) =>{
+        const userData = {
+            phone_number: PhoneNumber,
+            input_number : AuthNumber
+        };
+        const res = await axios.post('user/auth', userData);
+        
+        if (res.status === 200){
+            navigate(`/signup`);
+        }
+    }
+
+    const onCheckAuthNumber = ()=>{
+        checkAuthNumber(PhoneNumber, AuthNumber)
+    }
+
+    return(
+        <div className="inputBox">
+            <input className="form-control" type="text" value={PhoneNumber} 
+            onChange={onPhoneNumberHandler} placeholder="phone number"/>
+            {
+                IsSmsSended && (
+                    <input className="form-control" type="number" value={AuthNumber} 
+                        onChange={onAuthNumberHandler} placeholder="인증번호"/>
+                )
+            }
+            {
+                IsSmsSended && (<button className="btn btn-dark" onClick={onCheckAuthNumber}>인증번호 확인</button>)
+            }
+            {
+                !IsSmsSended && (<button className="btn btn-dark" onClick={onSendAuthNumber}>인증번호 받기</button>)
+            }
+        </div>
+    )
+}
+
+const SignupPage = ()=>{
+    const navigate = useNavigate();
+
+    const [PhoneNumber, setPhoneNumber] = useState("");
+    const [Nickname, setNickname] = useState("");
+    const [Password, setPassword] = useState("");
+    const [Password2, setPassword2] = useState("");
+
+    const onPhoneNumberHandler = (event) => {
+        setPhoneNumber(event.currentTarget.value);
+    }
+
+     const onNicknameHandler = (event) => {
+        setNickname(event.currentTarget.value);
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+
+    const onPassword2Handler = (event) => {
+        setPassword2(event.currentTarget.value);
+    }
+
+    const singup = async(PhoneNumber, Nickname, Password, Password2)=>{
+        const body = {
+            phone_number: PhoneNumber,
+            nickname: Nickname,
+            password: Password,
+            password2: Password2
+        }
+        const res = await axios.put('/user/signup', body);
+        if (res.status===200){
+            navigate('/login');
+        }
+    }
+
+    const onSignup = ()=>{
+        if(Password !== Password2){
+            alert("비밀번호가 일치하지 않습니다.");
+        } else {
+            singup(PhoneNumber, Nickname, Password, Password2);
+        }
+    }
+
+    return (
+        <div className="inputBox">
+            <input className="form-control" type="text" value={PhoneNumber} 
+            onChange={onPhoneNumberHandler} placeholder="phone number"/>
+            <input className="form-control" type="text" value={Nickname} 
+            onChange={onNicknameHandler} placeholder="nickname"/>
+            <input className="form-control" type="password" value={Password} 
+            onChange={onPasswordHandler} placeholder="password"/>
+            <input className="form-control" type="password" value={Password2} 
+            onChange={onPassword2Handler} placeholder="check password"/>
+            <button className="btn btn-dark" >로그인</button>
+            <button className="btn btn-dark" onClick={onSignup}>회원 가입</button>
+        </div>
+    )
+}
+
+export {LoginPage, SignupPage, AuthPage};
