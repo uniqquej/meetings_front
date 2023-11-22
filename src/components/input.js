24953d363ba08508f13863a97d6aecbf68e0b2a0
uniqquej <cjy4477@gmail.com>
2023-11-22 11:07:25 +0900

    feat: react-calender 적용

--- src/components/input.js
+++ src/components/input.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const InputBox = (probs)=>{
     return (
-        <div class="mb-3 row">
-            <label for={probs.name} class="col-sm-2 col-form-label text-center">{probs.labelName}</label>
+        <div className="mb-3 row">
+            <label htmlFor={probs.name} className="col-sm-2 col-form-label text-center">{probs.labelName}</label>
             {probs.readOnly ?
-                (<div class="col-sm-10">
-                    <input type={probs.type} id={probs.name} class="form-control" value={probs.value} onChange={probs.onChange} readonly/>
+                (<div className="col-sm-10">
+                    <input type={probs.type} id={probs.name} className="form-control" value={probs.value} onChange={probs.onChange} readOnly/>
                 </div>)
                 :
-                (<div class="col-sm-10">
-                    <input type={probs.type} id={probs.name} class="form-control" 
+                (<div className="col-sm-10">
+                    <input type={probs.type} id={probs.name} className="form-control" 
                     value={probs.value} onChange={probs.onChange} placeholder={probs.placeholder}/>
                 </div>)   
             }
--- src/group/group.js
+++ src/group/group.js
@@ -3,6 +3,9 @@ import moment from "moment";
 import React,{useState,useEffect} from "react";
 import {useParams, useNavigate} from "react-router-dom"
 
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 import "../group/group.css"
 import InputBox from "../components/input";
 import {NewNoticeModal,NoticeDetailModal} from "../components/modal";
@@ -13,6 +16,7 @@ const GroupDetail = ()=>{
     const {groupId} = useParams();
     const [data, setData] = useState("");
     const [meetings, setMeetings] = useState([]);
+    const [dates, setDates] = useState([]);
     const [notices, setNotices] = useState([]);
     const [toDoList, setToDoList] = useState([]);
 
@@ -22,9 +26,15 @@ const GroupDetail = ()=>{
         }).then(response=>{
             console.log(response)
             setData(response.data);
-            setMeetings(response.data.meeting_set);
+            setMeetings(response.data.calender_set);
             setNotices(response.data.notice_set);
             setToDoList(response.data.todolist_set);
+
+            const updatedDates = response.data.calender_set
+            .filter(day => day.meetings_cnt > 0)
+            .map(day => day.date);
+            setDates(updatedDates);
+
         }).catch(error=>{
             console.error(error);
         })
@@ -36,7 +46,7 @@ const GroupDetail = ()=>{
                 <button className="my-btn" onClick={()=>{navigate(`/chat/${data.group_name}`)}}>{data.group_name} 채팅방</button>
             </div>
             <Notice notices={notices} leader={data.leader}/>
-            <Meeting meetings={meetings}leader={data.leader}/>
+            <Meeting meetings={meetings} leader={data.leader} dates={dates}/>
             <ToDoList toDoList ={toDoList} />
         </div>
     )
@@ -76,20 +86,50 @@ const Notice = (probs)=>{
 
 const Meeting = (probs)=>{
     const userId = JSON.parse(localStorage.getItem('payload')).user_id;
-    return (
-        <div className="group-meeting text-center">
-            <div className="sub-title text-center">
-               <h4>meeting</h4>
-               {userId===probs.leader
-                ?(<button>+</button>)
-                :undefined
-               }
+    const [value, onChange] = useState(new Date());
+    const {meetings, leader, dates} = probs;
+
+  return (
+    <div className="group-meeting text-center">
+             <div className="sub-title text-center">
+                <h4>meeting</h4>
+                {userId===leader
+                 ?(<button>+</button>)
+                 :undefined
+                }
+             </div>
+             <div className="calender-box">
+                <Calendar
+                    className="calender"
+                    onChange={onChange}
+                    value={value}
+                    formatDay={(locale, date) => moment(date).format("DD")}
+                    tileContent={({ date, view }) => { 
+                        let html = [];
+                        if (dates.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
+                        html.push(<div className="dot"></div>);
+                        }
+                        return (
+                        <>{html}</>
+                        );
+                    }}
+                    />
+                <div className="detail-box">
+                    {meetings.filter(meetingInfo=>
+                        meetingInfo.date === moment(value).format("YYYY-MM-DD")
+                    )
+                    .map((data)=>(
+                        <div key={data.id}>
+                            <h5><b>세부 일정</b></h5>
+                            {data.meeting_set.map(data =>(
+                                <p key={data.id}>{moment(data.time,'HH:mm:ss').format('HH:mm')} | {data.title}</p>
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
-            {probs.meetings.map((meeting)=>(
-                <button className="list-btn" key={meeting.id}>{moment(meeting.time_to_meet).format("YYYY-MM-DD HH:MM")} | {meeting.title}</button>
-            ))}
         </div>
-    )
+  );
 }
 
 const ToDoList = (probs)=>{
